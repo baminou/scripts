85ec1eeae6747f1121783f848f316297bf703bec
Brice Aminou <brice.aminou@gmail.com>
2018-02-07 10:14:41 -0500

    Publish added to upload with song

--- upload_with_song.py
+++ upload_with_song.py
@@ -64,8 +64,10 @@ def main():
 
     subprocess.check_output(['icgc-storage-client','upload','--manifest',os.path.join(os.getcwd(),manifest_filename), '--force'])
 
-    #client.publish()
-
+    publish_response = api.publish(client.analysis_id)
+    requests.put('http://10.10.0.210:8080/studies/PAEN-AU/analysis/publish/ec575f11-0c17-11e8-8020-7f471767d4bb',
+                 headers={"Accept": "application/json", "Content-Type": "application/json",
+                          "Authorization": "Bearer d89fb4dd-19a4-40dd-acf5-eafcc8bca80b"})
 
 if __name__ == "__main__":
     main()
